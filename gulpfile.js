22b5ddc784cc2148b00ae5962db08900a2c07e7c
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2017-07-19 16:33:56 +0300

    Some changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ const cleanCSS = require('gulp-clean-css')
 const concat = require('gulp-concat')
 const del = require('del')
 const browserSync = require('browser-sync').create()
-const pug = require('gulp-pug')
+const jade = require('gulp-jade')
 const sass = require('gulp-sass')
 
 const destName = 'public'
 
  // cборка шаблонов
 gulp.task('templates', function buildHTML () {
-  return gulp.src('./source/pages/*.pug')
-    .pipe(pug({
+  return gulp.src('./source/pages/*.jade')
+    .pipe(jade({
         // Your options in here.
     }))
     .pipe(gulp.dest(destName))
 })
 
 // cборка стилей
-gulp.task('sass', function () {
+gulp.task('style', function () {
     // в source/styles сразу берет style.scss
   return gulp.src('./source/style/style.scss')
         .pipe(sass()) // переводит sass в обычный css
@@ -44,7 +44,7 @@ gulp.task('clean', function () {
 gulp.task('watch', function () {
   gulp.watch('source/**/*.scss', gulp.series('style'))
   gulp.watch('source/assets/**/*.*', gulp.series('assets'))
-  gulp.watch('source/**/*.pug', gulp.series('templates'))
+  gulp.watch('source/**/*.jade', gulp.series('templates'))
 })
 
 gulp.task('serve', function () {
